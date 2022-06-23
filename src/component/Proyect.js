81d50e0ff1724bc28a09e4054d360daeb2d60c7e
tayson87 <juanpablomartinez37@gmail.com>
2022-06-22 20:06:13 -0500

    main in progress

--- src/component/Proyect.js
+++ src/component/Proyect.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Menu from './Menu';
 
 const Proyect = () => {
     return (
         <div>
-            <h1> PROYECT </h1>
+            <div className='project-main'>
+            <Menu />
+            <span className='title-project'> PROYECTOS</span>
+            <span className='title-name'>JUAN PABLO</span>
+            </div>
+            <div className='app-time'>                
+                <p className='title-time'>App Clima</p>
+                <p className='description-time'>App del clima  se creo  consumiendo  una api 
+                publica en donde la labor es consultar la temperatura  dependiendo  de  la ubicacion en la que se encuentra   se  usaron  tecnologias como Reac, JS, Css</p> 
+
+                <button className='btn_time' onClick={'https://keen-allen-54f279.netlify.app/'}>Desplegar</button>
+                            
+                </div>
+
+                {/* <div className='logojs'>   
+                 <img src='../imagen/js.png' alt='' />  
+                </div>  */}
 
                
         </div>
